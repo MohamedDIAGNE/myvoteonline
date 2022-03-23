0b5b76ddd15ebd6211f68a85351f948d7d3f7129
Mohamed Diagne <mohameddiagne@MacBook-Air-de-Mohamed.local>
2022-03-23 01:15:09 +0100

    Procfile

--- election/urls.py
+++ election/urls.py
@@ -5,7 +5,6 @@ from .views import *
 app_name="election"
 
 urlpatterns = [
-    path(r"", liste_candidats, name="liste_candidats"),
     path("candidat/liste", liste_candidats, name="liste_candidats"),
     path("candidat/detail/<int:id_candidat>", detail_candidat, name="detail_candidat"),
     path("candidat/ajout", ajout_candidat, name="ajout_candidat"),
